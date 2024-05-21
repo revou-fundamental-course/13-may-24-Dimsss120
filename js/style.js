9f6f365364395000812f18424f323b43b13cb14f
Viend223 <dimasalfarisky89@gmail.com>
2024-05-22 07:26:58 +0800

    add css

---
+++ js/style.js
@@ -0,0 +1,52 @@
+// POP UP START // POP UP START // POP UP START // POP UP START
+
+let welcome = 1
+let perkenalan = 1
+let hormat = 1
+
+// switch welcome
+switch(welcome) {
+  case 1 :
+    welcome = 'Halo Selamat Datang Di MiniCoding Saya 😄'
+}
+
+// switch perkenalan
+switch(perkenalan) {
+  case 1 :
+    perkenalan = 'Baiklah.. Saya Dimas, Saya Berasal Dari Sulawesi Selatan Tepatnya Di Kota Makassar ❤️'
+}
+
+// switch hormat
+switch(hormat) {
+  case 1 :
+    hormat = 'Terima Kasih Telah Mengunjungi Web Kecil Saya :D'
+}
+
+// alert 1
+alert(`${welcome}`)
+
+// alert 2
+alert(`${perkenalan}`)
+
+// alert 3
+alert(`${hormat}`)
+
+//POP UP END //POP UP END //POP UP END //POP UP END //POP UP END
+
+
+
+// VALIDASI START
+// Luas Segitiga
+let alas = 32
+let tinggi = 17
+const a = 0.5
+  const hasil = a * alas * tinggi
+
+// Keliling Segitiga
+let s1 = 32
+let s2 = 29
+let s3 = 17
+  const akhir = s1 + s2 + s3
+
+// VALIDASI END
+
