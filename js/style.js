68d6639314b3c29e7dbe2b70560f144c5a27c217
Viend223 <dimasalfarisky89@gmail.com>
2024-05-23 10:23:15 +0800

    add js

--- js/style.js
+++ js/style.js
@@ -1,38 +1,3 @@
-// POP UP START // POP UP START // POP UP START // POP UP START
-
-// let welcome = 1
-// let perkenalan = 1
-// let hormat = 1
-
-// // switch welcome
-// switch(welcome) {
-//   case 1 :
-//     welcome = 'Halo Selamat Datang Di MiniCoding Saya 😄'
-// }
-
-// // switch perkenalan
-// switch(perkenalan) {
-//   case 1 :
-//     perkenalan = 'Baiklah.. Saya Dimas, Saya Berasal Dari Sulawesi Selatan Tepatnya Di Kota Makassar ❤️'
-// }
-
-// // switch hormat
-// switch(hormat) {
-//   case 1 :
-//     hormat = 'Terima Kasih Telah Mengunjungi Web Kecil Saya :D'
-// }
-
-// // alert 1
-// alert(`${welcome}`)
-
-// // alert 2
-// alert(`${perkenalan}`)
-
-// // alert 3
-// alert(`${hormat}`)
-
-//POP UP END //POP UP END //POP UP END //POP UP END //POP UP END
-
 
 // variabel
 
